7b10b8ee2012ac40d18df5f00609a414feef1710
dans-art <spy15@bluewin.ch>
2021-10-04 01:04:16 +0200

    Fixed "Send email" checkbox display.
    - The "Send email to new customer" will now be displayed if the "Add customer" checkbox is preselected.

--- include/js/wac-main-script.js
+++ include/js/wac-main-script.js
@@ -3,7 +3,6 @@
  * File description: Various Javascript functions.
  * Author: Dan's Art
  * Author URI: http://dev.dans-art.ch
- *
  */
 let wac_scripts = {
 
@@ -12,12 +11,24 @@ let wac_scripts = {
          * Fires on change of the wac checkbox. If selected, the "Notify customer" checkbox will be shown.
          */
         jQuery('#wac_add_customer').change(() => {
-            if (jQuery('#wac_add_customer').is(":checked")) {
-                jQuery('p.wac_add_customer_notify_field').show();
-            } else {
-                jQuery('p.wac_add_customer_notify_field').hide();
-            }
+            this.wac_checkbox_checker();
         });
+
+        //Run after document loaded to show the second checkbox.
+        jQuery(document).ready(function(){
+            wac_scripts.wac_checkbox_checker();
+        });
+    },
+
+    /**
+     * Checks if the secondary checkbox for the notification has to be shown or not.
+     */
+    wac_checkbox_checker(){
+        if (jQuery('#wac_add_customer').is(":checked")) {
+            jQuery('p.wac_add_customer_notify_field').show();
+        } else {
+            jQuery('p.wac_add_customer_notify_field').hide();
+        }
     }
 
 };
